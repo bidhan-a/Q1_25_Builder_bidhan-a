24f08136f8839d1b7dd7f46c7450116ba4d1892b
bidhan-a <bidhan619@gmail.com>
2025-01-16 21:51:27 +0545

    update nft functions

--- solana-starter/ts/cluster1/nft_image.ts
+++ solana-starter/ts/cluster1/nft_image.ts
@@ -1,11 +1,15 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import wallet from "../dev-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
@@ -14,17 +18,19 @@ umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        //1. Load image
-        //2. Convert image to generic file.
-        //3. Upload image
+  try {
+    //1. Load image
+    const image = await readFile("images/generug.png");
 
-        // const image = ???
+    //2. Convert image to generic file.
+    const nft_image = createGenericFile(image, "generug", {
+      contentType: "image/png",
+    });
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    //3. Upload image
+    const [myUri] = await umi.uploader.upload([nft_image]);
+    console.log("Your image URI: ", myUri); // Your image URI:  https://arweave.net/EKGmwbCHw8QkPzmcgqxMw9jErTGCmcG5uoATZ9y3QsWY
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
