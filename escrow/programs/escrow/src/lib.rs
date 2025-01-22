d55621b2816830dd20341139b639fbb703e39d12
bidhan-a <bidhan619@gmail.com>
2025-01-22 21:23:55 +0545

    add escrow program

---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+
+declare_id!("DY3vDNvABPVGZZrd2rtbF8bvoJ5woHRgc7SaqsTMqxbR");
+
+pub mod instructions;
+pub use instructions::*;
+
+pub mod state;
+pub use state::*;
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn make(
+        ctx: Context<Make>,
+        seed: u64,
+        receive_amount: u64,
+        deposit_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .init_escrow_state(seed, receive_amount, ctx.bumps)?;
+        ctx.accounts.deposit(deposit_amount)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.withdraw()?;
+        Ok(())
+    }
+}
