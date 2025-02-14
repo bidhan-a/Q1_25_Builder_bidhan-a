0803e7d51a5595e419ca5512ed559d220cfbec9c
bidhan-a <bidhan619@gmail.com>
2025-02-14 12:32:44 +0545

    add dice game

---
+++ dice-game/programs/dice-game/src/lib.rs
@@ -0,0 +1,27 @@
+use anchor_lang::prelude::*;
+
+declare_id!("D87NBxKbtZDFiXd9nnuiE19wK1KBnteTirttvrFAzJNz");
+
+mod instructions;
+mod state;
+
+use instructions::*;
+
+#[program]
+pub mod dice_game {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        ctx.accounts.init(amount)
+    }
+
+    pub fn place_bet(ctx: Context<PlaceBet>, seed: u128, roll: u8, amount: u64) -> Result<()> {
+        ctx.accounts.create_bet(seed, roll, amount, &ctx.bumps)?;
+        ctx.accounts.deposit(amount)
+    }
+
+    pub fn resolve_bet(ctx: Context<ResolveBet>, sig: Vec<u8>) -> Result<()> {
+        ctx.accounts.verify_ed25519_signature(&sig)?;
+        ctx.accounts.resolve_bet(&ctx.bumps, &sig)
+    }
+}
